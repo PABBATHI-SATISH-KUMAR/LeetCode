07f4f620cb0958ebd6e36eb944098c6864151a97
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2024-07-17 08:09:48 +0530

    Time: 4 ms (100%), Space: 43.8 MB (66.85%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(int n) {
+        boolean isPalo = false;
+        int rem,temp,rev;
+        temp = n;
+        rev=0;
+        if(n<0){
+            return false;
+        }
+        while(n>0){
+                rem = n%10;
+                rev = rev*10+rem;
+                n = n/10;
+        }
+        if(temp==rev){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+}
