32a2f03ce3b0ab82e1ed23eea588e7a0d158df17
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2024-07-30 23:44:18 +0530

    Time: 144 ms (5.09%), Space: 45.6 MB (86.61%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,40 +1,40 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        // int nonZeroIndex = 0;
-        // for (int i = 0; i < nums.length; i++) {
-        //     if (nums[i] != 0) {
-        //         nums[nonZeroIndex] = nums[i];
-        //         nonZeroIndex++;
-        //     }
-        // }
-        // for (int i = nonZeroIndex; i < nums.length; i++) {
-        //     nums[i] = 0;
-        // }
-        // for (int num : nums) {
-        //     System.out.print(num + " ");
-        // }
-        int j = -1;
-        // Find the first zero element
+        int nonZeroIndex = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                j = i;
-                break;
-            }
-        }
-        if (j == -1) {
-            return;
-        }
-        for (int i = j + 1; i < nums.length; i++) {
             if (nums[i] != 0) {
-                // Swap non-zero element with zero at index j
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                j++; // Increment j to point to the next zero
+                nums[nonZeroIndex] = nums[i];
+                nonZeroIndex++;
             }
         }
-         for (int num : nums) {
+        for (int i = nonZeroIndex; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+        for (int num : nums) {
             System.out.print(num + " ");
         }
+        // int j = -1;
+        // // Find the first zero element
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] == 0) {
+        //         j = i;
+        //         break;
+        //     }
+        // }
+        // if (j == -1) {
+        //     return;
+        // }
+        // for (int i = j + 1; i < nums.length; i++) {
+        //     if (nums[i] != 0) {
+        //         // Swap non-zero element with zero at index j
+        //         int temp = nums[i];
+        //         nums[i] = nums[j];
+        //         nums[j] = temp;
+        //         j++; // Increment j to point to the next zero
+        //     }
+        // }
+        //  for (int num : nums) {
+        //     System.out.print(num + " ");
+        // }
     }
-    }
+}
