59f94142ee3d3b01a2d5f6cbc362cf50276f0829
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2024-08-05 18:52:22 +0530

    Time: 1423 ms (5.05%), Space: 52.7 MB (64.11%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int majorityElement(int[] v) {
+
+        //Brute Force     O(N2), O(1)
+        int n = v.length;
+
+        for (int i = 0; i < n; i++) {
+            //selected element is v[i]
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                // counting the frequency of v[i]
+                if (v[j] == v[i]) {
+                    cnt++;
+                }
+            }
+
+            // check if frquency is greater than n/2:
+            if (cnt > (n / 2))
+                return v[i];
+        }
+
+        return -1;
+
+
+        
+    }
+}
