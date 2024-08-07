0d7415dff96d1ccde8a43a8f04e0d01e42850531
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2024-08-07 11:52:01 +0530

    Time: 2 ms (98.08%), Space: 45.2 MB (8.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,33 +1,50 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] ans = new int[2];
-        ans[0] = ans[1] = -1;
-        int[] original = nums.clone(); // store the original array
-        Arrays.sort(nums);
-        int left = 0, right = nums.length - 1;
-        while (left < right) {
-            int sum = nums[left] + nums[right];
-            if (sum == target) {
-                // find the indices in the original array
-                for (int i = 0; i < original.length; i++) {
-                    if (original[i] == nums[left]) {
-                        ans[0] = i;
-                        break;
-                    }
-                }
-                for (int i = 0; i < original.length; i++) {
-                    if (original[i] == nums[right] && i != ans[0]) {
-                        ans[1] = i;
-                        break;
-                    }
-                }
-                return ans;
-            } else if (sum < target) left++;
-            else right--;
+        // int[] ans = new int[2];
+        // ans[0] = ans[1] = -1;
+        // int[] original = nums.clone(); // store the original array
+        // Arrays.sort(nums);
+        // int left = 0, right = nums.length - 1;
+        // while (left < right) {
+        //     int sum = nums[left] + nums[right];
+        //     if (sum == target) {
+        //         // find the indices in the original array
+        //         for (int i = 0; i < original.length; i++) {
+        //             if (original[i] == nums[left]) {
+        //                 ans[0] = i;
+        //                 break;
+        //             }
+        //         }
+        //         for (int i = 0; i < original.length; i++) {
+        //             if (original[i] == nums[right] && i != ans[0]) {
+        //                 ans[1] = i;
+        //                 break;
+        //             }
+        //         }
+        //         return ans;
+        //     } else if (sum < target) left++;
+        //     else right--;
+        // }
+        // return ans;
+
+    
+        HashMap<Integer,Integer> map =new HashMap<>();
+        int ans[]=new int[2];
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                ans[0]=map.get(target-nums[i]);
+                ans[1]=i;
+                break;
+            }
+            else{
+                map.put(nums[i],i);
+            }
         }
-        return ans;
-
-
+            return ans;
         
     }
 }
+
+
+        
+    
