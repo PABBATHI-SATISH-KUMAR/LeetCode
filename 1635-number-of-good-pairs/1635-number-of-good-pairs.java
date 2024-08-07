f24a52504d5ffe08eef56089d643c443583a111e
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2024-08-07 12:00:10 +0530

    Time: 1 ms (87.68%), Space: 40.8 MB (72.36%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.java
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.java
@@ -1,11 +1,30 @@
+// class Solution {
+//     public int numIdenticalPairs(int[] nums) {
+//         int pc = 0;
+//         for(int i = 0; i<nums.length;i++){
+//             for(int j = i+1;j<nums.length;j++){
+//                 if(nums[i] == nums[j]) pc++;
+//             }
+//         }
+//         return pc;
+//     }
+// }
+
+
+import java.util.HashMap;
+
 class Solution {
     public int numIdenticalPairs(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
         int pc = 0;
-        for(int i = 0; i<nums.length;i++){
-            for(int j = i+1;j<nums.length;j++){
-                if(nums[i] == nums[j]) pc++;
+        
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                pc += map.get(num); // Add the current count of this number to the pair count
             }
+            map.put(num, map.getOrDefault(num, 0) + 1); // Update the frequency
         }
+        
         return pc;
     }
 }
