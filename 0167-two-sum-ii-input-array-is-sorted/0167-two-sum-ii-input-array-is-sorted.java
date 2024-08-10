e2433d9f2e02b536500e636b8d905ea1f7c82c5f
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2024-08-10 15:57:35 +0530

    Time: 0 ms (100%), Space: 47 MB (48.48%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,15 +1,42 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        int[] ans = new int[2];
-        for(int i = 0; i < numbers.length; i++) {
-            for(int j = i + 1; j < numbers.length; j++) {
-                if(numbers[i] + numbers[j] == target) {
-                    ans[0] = i + 1;
-                    ans[1] = j + 1;
-                    return ans;
+    public int[] twoSum(int[] nums, int target) {
+        // int[] ans = new int[2];
+        // for(int i = 0; i < nums.length; i++) {
+        //     for(int j = i + 1; j < nums.length; j++) {
+        //         if(nums[i] + nums[j] == target) {
+        //             ans[0] = i + 1;
+        //             ans[1] = j + 1;
+        //             return ans;
+        //         }
+        //     }
+        // }
+        // return new int[] {-1, -1};
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (true) {
+            int currentSum = nums[start] + nums[end];
+
+            if (currentSum == target) {
+                return new int[] {start + 1, end + 1}; 
+            }
+
+            int middle = (start + end) / 2;
+
+            if (currentSum < target) {
+                if (nums[middle] + nums[end] < target) {
+                    start = middle;
+                } else {
+                    ++start;
+                }
+            } else {
+                if (nums[middle] + nums[start] > target) {
+                    end = middle;
+                } else {
+                    --end;
                 }
             }
         }
-        return new int[] {-1, -1};
     }
 }
