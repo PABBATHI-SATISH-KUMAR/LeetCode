ae87ddb5886d7671669c36b7f33b4947e30c25ca
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2024-08-13 14:52:21 +0530

    Time: 2 ms (86.22%), Space: 55.1 MB (86.93%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -13,5 +13,19 @@ class Solution {
         }
 
         return ans;
+        // int n = nums.length;
+        // int[] ans = new int[n];
+
+        // for (int i = 0; i < n; i++) {
+        //     int product = 1;
+        //     for (int j = 0; j < n; j++) {
+        //         if (i != j) {
+        //             product *= nums[j];
+        //         }
+        //     }
+        //     ans[i] = product;
+        // }
+
+        // return ans;
     }
 }
