9e21381ac8d32c310b8b2fcb93dc6fe680b1f2f4
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2024-08-26 22:02:01 +0530

    Time: 2 ms (100%), Space: 57.1 MB (53.99%) - LeetHub

--- 3186-minimum-sum-of-mountain-triplets-ii/3186-minimum-sum-of-mountain-triplets-ii.java
+++ 3186-minimum-sum-of-mountain-triplets-ii/3186-minimum-sum-of-mountain-triplets-ii.java
@@ -1,32 +1,33 @@
 class Solution {
     public int minimumSum(int[] nums) {
-        int n = nums.length;
-        if (n < 3) return -1;
-        int[] leftMin = new int[n];
-        int[] rightMin = new int[n];
-        Arrays.fill(leftMin, Integer.MAX_VALUE);
-        Arrays.fill(rightMin, Integer.MAX_VALUE);
-        for (int i = 1; i < n; i++) {
-            if (nums[i - 1] < nums[i]) {
-                leftMin[i] = Math.min(leftMin[i - 1], nums[i - 1]);
-            } else {
-                leftMin[i] = leftMin[i - 1];
+        int[] pre = new int[nums.length];
+        int[] suf = new int[nums.length];
+        int n=nums.length;
+        if(n ==  0 ) return -1;
+         int prev=nums[0];
+        for(int i=0;i<n-1;i++)
+        {
+            prev=Math.min(prev,nums[i]);
+            pre[i]=prev;
+        }
+        prev=nums[n-1];
+        for(int i=n-1;i>=0;i--)
+        {
+            prev=Math.min(prev,nums[i]);
+            suf[i]=prev;
+        }
+        int c=Integer.MAX_VALUE;
+        for(int i=1;i<n-1;i++)
+        {
+            if(pre[i-1] < nums[i] )
+            {
+                if(suf[i+1] < nums[i])
+                {
+                    c = Math.min(c,pre[i]+suf[i]+nums[i]);
+                }
             }
         }
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i + 1] < nums[i]) {
-                rightMin[i] = Math.min(rightMin[i + 1], nums[i + 1]);
-            } else {
-                rightMin[i] = rightMin[i + 1];
-            }
-        }
-        int minSum = Integer.MAX_VALUE;
-        for (int j = 1; j < n - 1; j++) {
-            if (leftMin[j] < nums[j] && rightMin[j] < nums[j]) {
-                int sum = leftMin[j] + nums[j] + rightMin[j];
-                minSum = Math.min(minSum, sum);
-            }
-        }
-        return minSum == Integer.MAX_VALUE ? -1 : minSum;
+        if (c == Integer.MAX_VALUE) return -1;
+        return c;
     }
 }
