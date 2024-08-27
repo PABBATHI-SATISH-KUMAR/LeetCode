323186dfd71a4fea904881474b11edd32713da87
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2024-08-27 11:30:03 +0530

    Time: 23 ms (56.03%), Space: 11.6 MB (66.81%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def largestNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: str
+        """
+        return ''.join(sorted([str(num) for num in nums], key=lambda x: x*10,          reverse=True)) if set(nums)!=set([0]) else '0'
