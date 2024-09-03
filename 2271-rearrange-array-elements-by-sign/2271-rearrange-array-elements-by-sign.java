2d71ae4f7e458e12fbf1fbc4ff792c5999b64b6a
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2024-09-03 06:29:01 +0530

    Time: 3 ms (100%), Space: 76.7 MB (75.71%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,21 +1,35 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int[] positive = new int[nums.length / 2];
-        int[] negative = new int[nums.length / 2];
+        // int[] positive = new int[nums.length / 2];
+        // int[] negative = new int[nums.length / 2];
         
-        int posIndex = 0;
-        int negIndex = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] >= 0) {
-                positive[posIndex++] = nums[i];
-            } else {
-                negative[negIndex++] = nums[i];
+        // int posIndex = 0;
+        // int negIndex = 0;
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] >= 0) {
+        //         positive[posIndex++] = nums[i];
+        //     } else {
+        //         negative[negIndex++] = nums[i];
+        //     }
+        // }
+        // for (int i = 0; i < nums.length / 2; i++) {
+        //     nums[2 * i] = positive[i];
+        //     nums[2 * i + 1] = negative[i];
+        // }
+        // return nums;
+        int[] ans = new int[nums.length];
+        int pos = 0,neg = 1;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] < 0){
+                ans[neg] = nums[i];
+                neg+=2;
             }
+            else{
+                ans[pos] = nums[i];
+                pos+=2;
+            }
+            
         }
-        for (int i = 0; i < nums.length / 2; i++) {
-            nums[2 * i] = positive[i];
-            nums[2 * i + 1] = negative[i];
-        }
-        return nums;
+        return ans;
     }
 }
