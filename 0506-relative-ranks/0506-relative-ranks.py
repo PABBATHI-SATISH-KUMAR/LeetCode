0b7358f0d27a8aa6259fb2fa25bb5a3b396451b7
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2024-09-13 22:03:46 +0530

    Time: 59 ms (84.71%), Space: 17.6 MB (84.9%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        d={}
+        res=[]
+        descScore=sorted(score,reverse=True)
+        for i in range(len(descScore)):
+            if i==0:
+                d[descScore[i]]="Gold Medal"
+            elif i==1:
+                d[descScore[i]]="Silver Medal"
+            elif i==2:
+                d[descScore[i]]="Bronze Medal"
+            else:
+                d[descScore[i]]=str(i+1)
+
+        for i in range(len(score)):
+            res.append(d[score[i]])
+        return res
+        
