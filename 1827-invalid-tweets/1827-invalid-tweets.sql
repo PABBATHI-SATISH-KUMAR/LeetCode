4f9c3aceccf0fd8a895783eede9f07b8e7e11f09
PABBATHI SATISH KUMAR <pabbathisatishkumar@gmail.com>
2025-03-11 21:49:14 +0530

    Time: 900 ms (76.48%), Space: 0B (100%) - LeetHub

--- 1827-invalid-tweets/1827-invalid-tweets.sql
+++ 1827-invalid-tweets/1827-invalid-tweets.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
--- select tweet_id from Tweets where length(content) > 15 order by tweet_id ASC;
+ select tweet_id from Tweets where length(content) > 15 order by tweet_id ASC;
 
-SELECT tweet_id
-FROM Tweets
-WHERE CHAR_LENGTH(content)>15;
+-- SELECT tweet_id
+-- FROM Tweets
+-- WHERE CHAR_LENGTH(content)>15;
